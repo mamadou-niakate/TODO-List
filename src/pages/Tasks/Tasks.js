1cb095b417618cb2b214db50efab27806693f3c2
niakate <mamadouniakate382@gmail.com>
2021-10-30 22:28:22 +0200

    fixed sidebar bug fixed

--- src/pages/Tasks/Tasks.js
+++ src/pages/Tasks/Tasks.js
@@ -1,18 +1,17 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../../components/HOC';
 import TaskCardList from '../../components/Tasks/TaskCardList';
+import './index.css';
 
 function Tasks() {
   const { state } = useContext(AppContext);
 
   return (
-      <div className="content-container" style={{display:'flex', flexDirection:'column', justifyContent:'space-between'}}>
-        <div className="content">
-            <TaskCardList tasks={state.tasksTodo} title={"Todo"} />
-            <TaskCardList tasks={state.tasksInProgress} title={"In Progress"} />
-            <TaskCardList tasks={state.tasksComplete} title={"Complete"} />
-        </div>
-      </div>
+    <div className="content">
+        <TaskCardList tasks={state.tasksTodo} title={"Todo"} />
+        <TaskCardList tasks={state.tasksInProgress} title={"In Progress"} />
+        <TaskCardList tasks={state.tasksComplete} title={"Complete"} />
+    </div>
   );
 }
 
