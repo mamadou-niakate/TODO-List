5ed137f92f30c7cc9d2ac8d720a34d26aea67e9e
niakate <mamadouniakate382@gmail.com>
2021-11-07 19:54:07 +0100

    scroll cards list when overflowing

--- src/data.js
+++ src/data.js
@@ -1,85 +1,89 @@
 export const tasks = [
   {
-    id:0,
-    title:"Task 1",
-    description:"this is my task 1",
-    date:1389878466730,
-    priority:"medium",
-    status: "complete"
+    title: 'Todo',
+    tasks: [
+      {
+        title: 'Task 1',
+        description: 'Description 1',
+        done: false,    
+        date:1389878466730,
+        priority:"medium",
+        status: "complete",
+      },
+      {
+        title: 'Task 2',
+        description: 'Description 2',
+        date:1389878466730,
+        done: false,
+        priority:"medium",
+        status: "complete",
+      },
+      {
+        title: 'Task 3',
+        description: 'Description 3',
+        date:1389878466730,
+        done: false,
+        priority:"medium",
+        status: "complete",
+      },
+    ],
   },
   {
-    id:1,
-    title:"Task 2",
-    description:"this is my task 2",
-    date:1389988926901,
-    priority:"high",
-    status: "in progress"
+    title: 'In Progress',
+    tasks: [
+      {
+        title: 'Task 1',
+        description: 'Description 1',
+        done: false,
+        date:1389878466730,
+        priority:"medium",
+        status: "complete",
+      },
+      {
+        title: 'Task 2',
+        description: 'Description 2',
+        date:1389878466730,
+        done: false,
+        priority:"medium",
+        status: "complete",
+      },
+      {
+        title: 'Task 3',
+        description: 'Description 3',
+        date:1389878466730,
+        done: false,
+        priority:"medium",
+        status: "complete",
+      },
+    ],
   },
   {
-    id:2,
-    title:"Task 3",
-    description:"this is my task 3",
-    date:1389878466730,
-    priority:"medium",
-    status: "to do"
-  }
-  ,
-  {
-    id:3,
-    title:"Task 2",
-    description:"this is my task 2",
-    date:1389988926901,
-    priority:"medium",
-    status: "in progress"
+    title: 'Done',
+    tasks: [
+      {
+        title: 'Task 1',
+        description: 'Description 1',
+        done: false,
+        date:1389878466730,
+        priority:"medium",
+        status: "complete",
+      },
+      {
+        title: 'Task 2',
+        description: 'Description 2',
+        date:1389878466730,
+        done: false,
+        priority:"medium",
+        status: "complete",
+      },
+      {
+        title: 'Task 3',
+        description: 'Description 3',
+        date:1389878466730,
+        done: false,
+        priority:"medium",
+        status: "complete",
+      },
+    ],
   },
-  {
-    id:4,
-    title:"Task 3",
-    description:"this is my task 3",
-    date:1389878466730,
-    priority:"less",
-    status: "to do"
-  }
-  ,
-  {
-    id:5,
-    title:"Task 2",
-    description:"this is my task 2",
-    date:1389988926901,
-    priority:"high",
-    status: "to do"
-  },
-  {
-    id:6,
-    title:"Task 3",
-    description:"this is my task 3",
-    date:1389878466730,
-    priority:"less",
-    status: "complete"
-  },
-  {
-    id:7,
-    title:"Task 3",
-    description:"this is my task 3",
-    date:1389878466730,
-    priority:"medium",
-    status: "to do"
-  }
-  ,
-  {
-    id:8,
-    title:"Task 2",
-    description:"this is my task 2",
-    date:1389988926901,
-    priority:"high",
-    status: "to do"
-  },
-  {
-    id:9,
-    title:"Task 3",
-    description:"this is my task 2",
-    date:1389878466730,
-    priority:"high",
-    status: "complete"
-  }
 ]
