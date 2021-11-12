420069c268c4ec2489d55055e4e357972a1047ce
niakate <mamadouniakate382@gmail.com>
2021-11-12 15:19:00 +0100

    adjustement

--- src/pages/Tasks/Tasks.js
+++ src/pages/Tasks/Tasks.js
@@ -7,22 +7,24 @@ function Tasks() {
   const { state } = useContext(AppContext);
 
   return (
-    <div className={`content ${state.switchToDarkMode ? "content-dark" : "content-light"}`}>
-      {
-        state.tasks && state.tasks.map((tasksGroup) => {
-          if(tasksGroup !== null) {
-            const { title, tasks} = tasksGroup;
-            return (
-              <TaskCardList
-                key={Math.random() * 1000}
-                tasks={tasks || []}
-                title={title || ''}
-              />
-            )
-          }
-          return null;
-        })
-      }
+    <div className="tasks-page">
+      <div className={`content ${state.switchToDarkMode ? "content-dark" : "content-light"}`}>
+        {
+          state.tasks && state.tasks.map((tasksGroup) => {
+            if(tasksGroup !== null) {
+              const { title, tasks} = tasksGroup;
+              return (
+                <TaskCardList
+                  key={Math.random() * 1000}
+                  tasks={tasks || []}
+                  title={title || ''}
+                />
+              )
+            }
+            return null;
+          })
+        }
+      </div>
     </div>
   );
 }
